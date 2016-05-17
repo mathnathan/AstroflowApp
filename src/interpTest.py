e02174985634ef3f2ae77aae96f1d88055072a17
erlebach <gordon.erlebach@gmail.com>
2016-05-17 12:06:18 -0400

    Code demonstrating exact derivatives.

--- src/interpTest.py
+++ src/interpTest.py
@@ -3,10 +3,11 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 
 numKnots = 12
-numSamples = 100
+numSamples = 1000
 
 xpts = np.linspace(0,2*np.pi,numKnots)
 ypts = np.sin(xpts)
+exact_deriv = np.cos(xpts)
 
 plt.scatter(xpts, ypts, color='k', label="knots")
 
@@ -16,10 +17,23 @@ samplePts = np.array(interpolate.splev(sampleParams, tck, der=0))
 
 plt.plot(samplePts[0], samplePts[1], color='b', label='sin')
 
+# Finite-difference derivative based on the sample points
+# dy/dx = (dy/dp) / (dx/dp)
+x = samplePts[0]
+y = samplePts[1]
+derivs = np.zeros(len(x))
+derivs[1:-1] = (y[2:] - y[0:-2]) / (x[2:] - x[0:-2])
+derivs[0]    = (y[1] - y[0]) / (x[1] - x[0])
+derivs[-1]   = (y[-1] - y[-2]) / (x[-1] - x[-2])
+
+plt.plot(x, derivs, color='cyan', label='F-D deriv')
+plt.scatter(xpts, exact_deriv, color='black', label='exact deriv')
+
 derivs = np.array(interpolate.splev(sampleParams, tck, der=1))
+print derivs
 normDerivs = derivs / np.linalg.norm(derivs, axis=0) # Normalize the derivative
 
 plt.plot(samplePts[0], derivs[1], color='r', label='deriv')
-plt.plot(samplePts[0], normDerivs[1], color='g', label='norm deriv')
+#plt.plot(samplePts[0], normDerivs[1], color='g', label='norm deriv')
 plt.legend()
 plt.show()
